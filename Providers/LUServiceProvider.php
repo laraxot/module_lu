721b54cb91a861b11740387ef2077d4142b1bbe5
marco76tv <marco.sottana@gmail.com>
2019-08-05 22:31:12 +0200

    .

--- Providers/LUServiceProvider.php
+++ Providers/LUServiceProvider.php
@@ -17,4 +17,24 @@ class LUServiceProvider extends XotBaseServiceProvider{
     protected $module_dir= __DIR__;
     protected $module_ns=__NAMESPACE__;
     public $module_name='lu';
+
+    public function bootCallback(){
+    	//ddd('preso');
+    	/*
+    	if(!\Auth::check()) {
+        	//$userClass = config('auth.providers.users.model');
+        	$userClass= \Modules\LU\Models\User::class;
+        	\Auth::setUser(new $userClass());
+        	ddd(\Auth::user());
+    	}
+    	*/
+    }
 }
+
+/**
+* https://github.com/Idavoll/User/tree/2.0
+* https://laravel-news.com/laravel-model-events-getting-started
+* fireModelEvent
+* https://www.marknotes.fr/docs/Development/Web/Laravel/Slides/Event_Driven/index.html
+* https://blog.pusher.com/event-driven-laravel-applications/ 
+*/
