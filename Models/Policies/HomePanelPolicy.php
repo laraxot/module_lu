a3a8da1b38e68af0758b305321c532eee48eec47
marco76tv <marco.sottana@gmail.com>
2021-03-03 11:46:27 +0100

    .

--- Models/Policies/HomePanelPolicy.php
+++ Models/Policies/HomePanelPolicy.php
@@ -2,6 +2,7 @@
 
 namespace Modules\LU\Models\Panels\Policies;
 
+
 use Modules\Xot\Contracts\PanelContract;
 use Modules\Xot\Contracts\UserContract;
 use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
@@ -9,8 +10,8 @@ use Modules\Xot\Models\Panels\Policies\XotBasePanelPolicy;
 /**
  * Class HomePanelPolicy.
  */
-class HomePanelPolicy extends XotBasePanelPolicy {
-       public function test(UserContract $user, PanelContract $panel): bool {
+ class HomePanelPolicy extends XotBasePanelPolicy {
+    public function test(UserContract $user, PanelContract $panel): bool {
         return true;
     }
 
