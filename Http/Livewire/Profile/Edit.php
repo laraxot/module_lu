67065da11bcf97afe3fc1b4c1cd24dde181aa654
Malebestia <malebestia@yahoo.it>
2022-05-16 19:23:19 +0200

    up

--- Http/Livewire/Profile/Edit.php
+++ Http/Livewire/Profile/Edit.php
@@ -4,25 +4,24 @@ declare(strict_types=1);
 
 namespace Modules\LU\Http\Livewire\Profile;
 
-use Livewire\Component;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
 
 /**
  * Class Edit.
  */
 class Edit extends Component {
     //public Model $profile;
-   public array $form_data=[];
+    public array $form_data = [];
 
-    public function mount(Model $profile): void{
-        $this->form_data=$profile->toArray();
+    public function mount(Model $profile): void {
+        $this->form_data = $profile->toArray();
         //dddx($this->form_data);
     }
 
     public function render(): Renderable {
-
         $view = 'lu::livewire.profile.edit';
         $view_params = [
             'view' => $view,
@@ -31,8 +30,14 @@ class Edit extends Component {
         return view()->make($view, $view_params);
     }
 
-    public function save(): void{
+    public function save(): void {
+        //dddx([$this->form_data, Auth::user()->profile]);
+        $profile = Auth::user()->profile;
+        $profile->update($this->form_data);
 
-        dddx('eccolo');
+        $this->form_data['user']['email'] = $this->form_data['email'];
+        Auth::user()->update($this->form_data['user']);
+
+        dddx([$profile->toArray(), $this->form_data]);
     }
 }
--- Resources/views/livewire/profile/edit.blade.php
+++ Resources/views/livewire/profile/edit.blade.php
@@ -1,16 +1,18 @@
 <div>
-    <p class="text-sm text-muted"><i class="fa fa-id-card fa-fw me-2"></i>aaaa<br>
+    <p class="text-sm text-muted"><i class="fa fa-id-card fa-fw me-2"></i>{{ $form_data['user']['handle'] }}<br>
         {{-- <iclass="fafa-birthday-cakefa-fwme-2"></i>06/22/1980<br> --}}
-        <i class="fa fa-envelope-open fa-fw me-2"></i>the_email<span class="mx-2"> |
-        </span>
-        <i class="fa fa-phone fa-fw me-2"></i>12345678
+        <i class="fa fa-envelope-open fa-fw me-2"></i>{{ $form_data['email'] }}
+        @if (!empty($form_data['phone']))
+            <span class="mx-2"> | </span>
+            <i class="fa fa-phone fa-fw me-2"></i>{{ $form_data['phone'] }}
+        @endif
     </p>
-    <div class="collapse" id="personalDetails">
+    <div class="collapse" id="personalDetails" wire:ignore>
         <div class="row pt-4">
             <div class="mb-4 col-md-6">
                 <label class="form-label" for="name">Name</label>
                 {{-- <inputclass="form-control"type="text"name="name"id="name"value="JohnDoe"> --}}
-                <input class="form-control" type="text" {{-- wire:model="handle" --}}>
+                <input class="form-control" type="text" wire:model="form_data.user.handle">
             </div>
             {{-- <div class="mb-4 col-md-6">
                 <label class="form-label" for="date">Date of birth</label>
@@ -18,11 +20,11 @@
             </div> --}}
             <div class="mb-4 col-md-6">
                 <label class="form-label" for="email">Email address</label>
-                <input class="form-control" type="email" {{-- wire:model="email" --}}>
+                <input class="form-control" type="email" wire:model="form_data.email">
             </div>
             <div class="mb-4 col-md-6">
                 <label class="form-label" for="phone">Phone number</label>
-                <input class="form-control" type="text" {{-- wire:model="phone" --}}>
+                <input class="form-control" type="text" wire:model="form_data.phone">
             </div>
         </div>
         <button class="btn btn-outline-primary mb-4" wire:click="save()">
