22037d008a35cbdaa4177c1714f8209e1798b141
marco76tv <marco.sottana@gmail.com>
2022-05-16 13:15:28 +0200

    .

---
+++ Casts/UserField.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\LU\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class UserField implements CastsAttributes {
+    /**
+     * Cast the given value.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string                              $key
+     * @param mixed                               $value
+     * @param array                               $attributes
+     *
+     * @return \App\Models\Address
+     */
+    public function get($model, $key, $value, $attributes) {
+        return $model->user->{$key};
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @param string                              $key
+     * @param \App\Models\Address                 $value
+     * @param array                               $attributes
+     */
+    public function set($model, $key, $value, $attributes) {
+        $user = $model->user;
+        $user->{$key} = $value;
+        $res = tap($user)->save();
+    }
+}
