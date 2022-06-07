f9ad91bbb34cac24413cf73c27a60c6951d14446
marco xot <marco.sottana@gmail.com>
2022-06-07 17:51:36 +0200

    .

--- Models/OauthClient.php
+++ Models/OauthClient.php
@@ -9,10 +9,20 @@ use Laravel\Passport\Client as PassportClient;
 /**
  * Modules\LU\Models\OauthClient
  *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string $name
+ * @property string|null $secret
+ * @property string|null $provider
+ * @property string $redirect
+ * @property bool $personal_access_client
+ * @property bool $password_client
+ * @property bool $revoked
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\OauthAuthCode[] $authCodes
  * @property-read int|null $auth_codes_count
  * @property-read string|null $plain_secret
- * @property-write mixed $secret
  * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\OauthAccessToken[] $tokens
  * @property-read int|null $tokens_count
  * @property-read \Modules\LU\Models\User|null $user
@@ -20,6 +30,17 @@ use Laravel\Passport\Client as PassportClient;
  * @method static \Illuminate\Database\Eloquent\Builder|OauthClient newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|OauthClient newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|OauthClient query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient wherePasswordClient($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient wherePersonalAccessClient($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereProvider($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereRedirect($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereRevoked($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereSecret($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OauthClient whereUserId($value)
  * @mixin \Eloquent
  */
 class OauthClient extends PassportClient {
