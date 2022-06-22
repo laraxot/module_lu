b25eadb9c3556ec201a9d819b2ed0bf6f9dcc2e3
marco76tv <marco.sottana@gmail.com>
2022-06-22 13:56:22 +0200

    .

--- Charts/DailyUsersChart.php
+++ Charts/DailyUsersChart.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\LU\Charts;
 
 use Carbon\Carbon;
@@ -8,17 +10,13 @@ use Fidum\ChartTile\Charts\Chart;
 use Illuminate\Http\Request;
 
 /**
- * Undocumented class
+ * Undocumented class.
  */
 class DailyUsersChart extends Chart {
-
     /**
-     * Undocumented function
-     *
-     * @param Request $request
-     * @return Chartisan
+     * Undocumented function.
      */
-    public function handler(Request $request): Chartisan  {
+    public function handler(Request $request): Chartisan {
         $date = Carbon::now()->subMonth()->startOfDay();
 
         $data = collect(range(0, 12))->map(function ($days) use ($date) {
@@ -27,19 +25,20 @@ class DailyUsersChart extends Chart {
                 'y' => rand(100, 500),
             ];
         });
+        $labels = $data->pluck('x')->toArray();
+        $dataset = $data->toArray();
+        $labels[0] = $request->qid;
 
         return Chartisan::build()
-            ->labels($data->pluck('x')->toArray())
-            ->dataset('Example Data', $data->toArray());
+            ->labels($labels)
+            ->dataset('Example Data', $dataset);
     }
 
-    public function type(): string
-    {
+    public function type(): string {
         return 'bar';
     }
 
-    public function options(): array
-    {
+    public function options(): array {
         return [
             'responsive' => true,
             'maintainAspectRatio' => false,
