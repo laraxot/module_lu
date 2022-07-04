fc77f5e1b16dc831e4fd94f86a45c762bdb00430
marco xot <marco.sottana@gmail.com>
2022-07-04 10:48:28 +0200

    up

--- Models/Area.php
+++ Models/Area.php
@@ -12,6 +12,50 @@ use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 use Staudenmeir\EloquentHasManyDeep\HasRelationships;
 
+/**
+ * Modules\LU\Models\Area
+ *
+ * @property int $id
+ * @property int $application_id
+ * @property string $area_define_name
+ * @property string $db
+ * @property string $img
+ * @property string $icons
+ * @property int $ordine
+ * @property string $controller_path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property int|null $order_column
+ * @property-read string $guid
+ * @property-read bool|mixed|string $icon_src
+ * @property-read string|null $title
+ * @property-read \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\UrlGenerator|string $url
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\PermUser[] $permUsers
+ * @property-read int|null $perm_users_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\PermUser[] $perms
+ * @property-read int|null $perms_count
+ * @method static \Modules\LU\Database\Factories\AreaFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Area newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Area ordered(string $direction = 'asc')
+ * @method static \Illuminate\Database\Eloquent\Builder|Area query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereApplicationId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereAreaDefineName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereControllerPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereDb($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereIcons($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereImg($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereOrderColumn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereOrdine($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Area whereUpdatedBy($value)
+ * @mixin \Eloquent
+ */
 class Area extends BaseModel implements Sortable {
     use SortableTrait;
     use HasRelationships;
