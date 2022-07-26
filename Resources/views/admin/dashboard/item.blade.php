f538b8cac684a9396ed4a5c39bb0c0f57523a454
marco76tv <marco.sottana@gmail.com>
2022-07-26 09:06:38 +0200

    .

--- Resources/views/admin/dashboard/item.blade.php
+++ Resources/views/admin/dashboard/item.blade.php
@@ -12,7 +12,11 @@
                 </tr>
             </thead>
             <tbody>
+<<<<<<< HEAD
                 @foreach ($_theme->lastLoggedUsers(10) as $user)
+=======
+                @foreach ($_theme->lastLoggedUsers([10]) as $user)
+>>>>>>> 3d16a68 (.)
                     <tr>
                         <td>{{ $user->id }}<br />{{ $user->handle }}</td>
                         <td>{{ $user->first_name }}<br />{{ $user->last_name }}</td>
