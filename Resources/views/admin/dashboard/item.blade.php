4e07396f3142efd7e08aae5949887419d56a0752
marco76tv <marco.sottana@gmail.com>
2022-07-27 09:42:46 +0200

    rebase

--- Resources/views/admin/dashboard/item.blade.php
+++ Resources/views/admin/dashboard/item.blade.php
@@ -14,6 +14,7 @@
             <tbody>
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 @foreach ($_theme->lastLoggedUsers(10) as $user)
 =======
                 @foreach ($_theme->lastLoggedUsers([10]) as $user)
@@ -21,6 +22,9 @@
 =======
                 @foreach ($_theme->lastLoggedUsers(10) as $user)
 >>>>>>> d2787cb (up)
+=======
+                @foreach ($_theme->lastLoggedUsers([10]) as $user)
+>>>>>>> 3d16a68 (.)
                     <tr>
                         <td>{{ $user->id }}<br />{{ $user->handle }}</td>
                         <td>{{ $user->first_name }}<br />{{ $user->last_name }}</td>
