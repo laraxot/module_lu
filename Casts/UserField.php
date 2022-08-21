c2a0556c1f9c03ebc71d6e21f571c400e0d58eba
Malebestia <malebestia@yahoo.it>
2022-08-21 12:27:14 +0200

    .

--- Casts/UserField.php
+++ Casts/UserField.php
@@ -5,40 +5,46 @@ declare(strict_types=1);
 namespace Modules\LU\Casts;
 
 use Exception;
-use Modules\Xot\Contracts\ModelWithUserContract;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Modules\Xot\Contracts\ModelWithUserContract;
 
 class UserField implements CastsAttributes {
     /**
      * Cast the given value.
      *
      * param \Illuminate\Database\Eloquent\Model $model
-     * @param ModelWithUserContract                 $model
-     * @param string                                $key
-     * @param mixed                                 $value
-     * @param array                                 $attributes
+     *
+     * @param ModelWithUserContract $model
+     * @param string                $key
+     * @param mixed                 $value
+     * @param array                 $attributes
      *
      * @return mixed
      */
     public function get($model, $key, $value, $attributes) {
         // dddx(['model' => $model, 'key' => $key, 'value' => $value, 'attributes' => $attributes, 'data' => request()->all()]);
-        //Access to an undefined property Illuminate\Database\Eloquent\Model::$user.
+        // Access to an undefined property Illuminate\Database\Eloquent\Model::$user.
+        if (is_null($model->user)) {
+            return null;
+        }
+
         return $model->user->{$key};
     }
 
     /**
      * Prepare the given value for storage.
      *
-      * param \Illuminate\Database\Eloquent\Model $model
-     * @param ModelWithUserContract                 $model
-     * @param string                              $key
+     * param \Illuminate\Database\Eloquent\Model $model
+     *
+     * @param ModelWithUserContract $model
+     * @param string                $key
      * @param mixed                 $value
-     * @param array                               $attributes
+     * @param array                 $attributes
      */
     public function set($model, $key, $value, $attributes) {
-        //Access to an undefined property Illuminate\Database\Eloquent\Model::$user. 
+        // Access to an undefined property Illuminate\Database\Eloquent\Model::$user.
         $user = $model->user;
-        if($user==null){
+        if (null == $user) {
             return [];
             /*
             dddx([
@@ -50,13 +56,13 @@ class UserField implements CastsAttributes {
             throw new Exception('['.__LINE__.']['.__FILE__.']');
             */
         }
-        $up=[
+        $up = [
             $key => $value,
         ];
         $user->update($up);
-        //$user->{$key} = $value;
-        //Call to an undefined method Illuminate\Support\HigherOrderTapProxy<mixed>::save().  
-        //$res = tap($user)->save();
+        // $user->{$key} = $value;
+        // Call to an undefined method Illuminate\Support\HigherOrderTapProxy<mixed>::save().
+        // $res = tap($user)->save();
         // parent::__construct([]);
         // return [$key => encrypt($value)];
         // return ['created_by' => 'xot'];
