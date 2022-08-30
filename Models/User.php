82710ae160ffb5d7ee13e8d3bddb421286e4b0b9
marco xot <marco.sottana@gmail.com>
2022-08-30 18:28:46 +0000

    up

--- Models/User.php
+++ Models/User.php
@@ -21,6 +21,101 @@ use Modules\Xot\Traits\Updater;
 // use Spatie\Tags\HasTags;  // Spatie Tags
 use Staudenmeir\EloquentHasManyDeep\HasRelationships;
 
+/**
+ * Modules\LU\Models\User
+ *
+ * @property int $id
+ * @property string $handle
+ * @property string|null $passwd
+ * @property string|null $lastlogin
+ * @property int|null $owner_user_id
+ * @property int|null $owner_group_id
+ * @property string|null $is_active
+ * @property int|null $enable
+ * @property string|null $email
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property int|null $ente
+ * @property int|null $matr
+ * @property string|null $password
+ * @property string|null $two_factor_secret
+ * @property string|null $two_factor_recovery_codes
+ * @property string|null $hash
+ * @property string|null $activation_code
+ * @property string|null $forgotten_password_code
+ * @property \Illuminate\Support\Carbon|null $last_login_at
+ * @property string|null $last_login_ip
+ * @property string|null $token_check
+ * @property int|null $is_verified
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\AreaPermUser[] $areaPermUsers
+ * @property-read int|null $area_perm_users_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\OauthClient[] $clients
+ * @property-read int|null $clients_count
+ * @property-read string $full_name
+ * @property-read string $guid
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\GroupPermUser[] $groupPermUsers
+ * @property-read int|null $group_perm_users_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\GroupPermUser[] $groups
+ * @property-read int|null $groups_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Modules\LU\Models\PermUser|null $perm
+ * @property-read \Modules\LU\Models\PermUser|null $permUser
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\PermUserRight[] $permUserRights
+ * @property-read int|null $perm_user_rights_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\PermUser[] $permUsers
+ * @property-read int|null $perm_users_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\PermUser[] $perms
+ * @property-read int|null $perms_count
+ * @property-read \Modules\Mediamonitor\Models\Profile|null $profile
+ * @property-read \Modules\Mediamonitor\Models\Profile|null $profileOrCreate
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\PermUserRight[] $rights
+ * @property-read int|null $rights_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\SocialProvider[] $socialProviders
+ * @property-read int|null $social_providers_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Modules\LU\Models\OauthAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Modules\LU\Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereActivationCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedBy($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEnable($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEnte($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereForgottenPasswordCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereHandle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereHash($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsVerified($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLoginIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastlogin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereMatr($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereOwnerGroupId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereOwnerUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePasswd($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTokenCheck($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorRecoveryCodes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorSecret($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedBy($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable implements UserContract {
     use HasApiTokens;
     use HasFactory;
