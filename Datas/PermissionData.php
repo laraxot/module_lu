f11ed967c826808909dac8510b03993c0d1ebe01
Davide Cavallini <davidecavallini1987@gmail.com>
2023-01-03 17:12:16 +0100

    up

---
+++ Datas/PermissionData.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\LU\Datas;
+
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\DataCollection;
+
+class PermissionData extends Data {
+    public string $name;
+    
+}
---
+++ Datas/RoleData.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\LU\Datas;
+
+use Spatie\LaravelData\Data;
+use Modules\LU\Datas\PermissionData;
+use Spatie\LaravelData\DataCollection;
+
+class RoleData extends Data {
+    public string $name;
+    /**
+     * Summary of permissions
+     * @var DataCollection<PermissionData>
+     */
+    public DataCollection $permissions;
+}
