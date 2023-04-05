bcc0af2a1d8d09e5d2242dcdbde6b248d234efa4
marco xot <marco.sottana@gmail.com>
2023-04-05 17:36:31 +0200

    up

--- Http/Controllers/Auth/ResetPasswordController.php
+++ Http/Controllers/Auth/ResetPasswordController.php
@@ -6,39 +6,36 @@ namespace Modules\LU\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 // use Illuminate\Foundation\Auth\ResetsPasswords;
-//use Modules\LU\Traits\ResetsPasswords;
+// use Modules\LU\Traits\ResetsPasswords;
 // use App\Http\Controllers\Controller;
-//use Illuminate\Support\Str;
+// use Illuminate\Support\Str;
 // --------- Models ------------
-//use Illuminate\View\View;
-//use Modules\LU\Models\User;
-//use Modules\Xot\Contracts\UserContract;
-use Modules\Xot\Services\FileService;
-use Illuminate\Foundation\Auth\ResetsPasswords;
+// use Illuminate\View\View;
+// use Modules\LU\Models\User;
+// use Modules\Xot\Contracts\UserContract;
 use Modules\LU\Http\Controllers\BaseController;
-
+// va usato quello di LU altrimenti non valida come dovrebbe i cambi password
+use Modules\LU\Traits\ResetsPasswords;
+use Modules\Xot\Services\FileService;
 
 /**
  * Class ResetPasswordController.
  */
-class ResetPasswordController extends BaseController
-{
-
+class ResetPasswordController extends BaseController {
     use ResetsPasswords;
 
     protected string $redirectTo = '/';
 
-    public function showResetForm(Request $request)
-    {
+    public function showResetForm(Request $request) {
         $lang = app()->getLocale();
         $token = $request->route()->parameter('token');
 
         $piece = 'auth.passwords.reset';
-        FileService::viewCopy('lu::' . $piece, 'pub_theme::' . $piece);
+        FileService::viewCopy('lu::'.$piece, 'pub_theme::'.$piece);
         /**
          * @phpstan-var view-string
          */
-        $view = 'pub_theme::' . $piece;
+        $view = 'pub_theme::'.$piece;
         $view_params = [
             'token' => $token,
             'email' => $request->email,
@@ -49,17 +46,16 @@ class ResetPasswordController extends BaseController
         return view($view, $view_params);
     }
 
-     /**
+    /**
      * Set the user's password.
      *
-     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
-     * @param  string  $password
+     * @param \Illuminate\Contracts\Auth\CanResetPassword $user
+     * @param string                                      $password
+     *
      * @return void
      */
-    protected function setUserPassword($user, $password)
-    {
-        //$user->password = Hash::make($password);
-        $user->update(['passwd'=>$password]);
+    protected function setUserPassword($user, $password) {
+        // $user->password = Hash::make($password);
+        $user->update(['passwd' => $password]);
     }
-
 }
