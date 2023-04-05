f0508fdac341f3c2b54fabbd488531194e5c4298
Malebestia <vair81@gmail.com>
2023-04-05 16:21:35 +0200

    up

--- Actions/BuildUserMailMessageAction.php
+++ Actions/BuildUserMailMessageAction.php
@@ -12,40 +12,24 @@ use Spatie\QueueableAction\QueueableAction;
 class BuildUserMailMessageAction {
     use QueueableAction;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function execute(string $name, array $view_params): MailMessage
-=======
-    public function execute(string $name, array $view_params)
->>>>>>> cf94a76 (up)
-    {
-
-=======
     public function execute(string $name, array $view_params): MailMessage {
->>>>>>> b04bb5c (up)
         $theme = NotifyTheme::firstOrCreate([
             'lang' => $view_params['lang'],
             'type' => 'email', // email,sms,whatsapp,piccione
             'post_type' => $name,
             'post_id' => $view_params['post_id'] ?? 0,
-<<<<<<< HEAD
-=======
-            'post_id' => $view_params['post_id'],
->>>>>>> cf94a76 (up)
-=======
->>>>>>> b04bb5c (up)
         ]);
-        if (null == $theme->subject) {
+        if (null === $theme->subject) {
             $subject = trans('lu::auth.'.$name.'.subject');
             $theme->update(['subject' => $subject]);
         }
-        if (null == $theme->theme) {
+        if (null === $theme->theme) {
             $theme->update(['theme' => 'ark']);
         }
-        if (null == $theme->body_html) {
+        if (null === $theme->body_html) {
             $html = trans('lu::auth.'.$name.'.body_html');
 
-            if ('verify-email' == $name && 3 == $view_params['post_id']) {
+            if ('verify-email' === $name && 3 === $view_params['post_id']) {
                 $html .= '<br/>When you\'ll re-login this will be your password: ##password##';
             }
 
@@ -56,7 +40,7 @@ class BuildUserMailMessageAction {
 
         $body_html = $theme->body_html;
         foreach ($view_params as $k => $v) {
-            if (is_string($v)) {
+            if (\is_string($v)) {
                 $body_html = Str::replace('##'.$k.'##', $v, $body_html);
             }
         }
--- Http/Controllers/Auth/ForgotPasswordController.php
+++ Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,54 +4,33 @@ declare(strict_types=1);
 
 namespace Modules\LU\Http\Controllers\Auth;
 
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Modules\Xot\Services\FileService;
 use Modules\LU\Http\Controllers\BaseController;
-use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
-
-<<<<<<< HEAD
-=======
-use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
->>>>>>> cf94a76 (up)
-=======
->>>>>>> b04bb5c (up)
+use Modules\Xot\Services\FileService;
 
 /**
  * Class ForgotPasswordController.
  */
-class ForgotPasswordController extends BaseController
-{
+class ForgotPasswordController extends BaseController {
     use SendsPasswordResetEmails;
 
-
     protected string $redirectTo = '/';
-
 
     /**
      * Display the form to request a password reset link.
      *
      * @return \Illuminate\View\View
      */
-    public function showLinkRequestForm()
-    {
-
+    public function showLinkRequestForm() {
         $lang = app()->getLocale();
-<<<<<<< HEAD
-=======
-        $lang = app()->getLocale();
-
-
->>>>>>> cf94a76 (up)
-=======
->>>>>>> b04bb5c (up)
         $piece = 'auth.passwords.email';
-        FileService::viewCopy('lu::' . $piece, 'pub_theme::' . $piece);
+        FileService::viewCopy('lu::'.$piece, 'pub_theme::'.$piece);
 
         /**
          * @phpstan-var view-string
          */
-        $view = 'pub_theme::' . $piece;
+        $view = 'pub_theme::'.$piece;
         $view_params = [
             'lang' => $lang,
             'title' => 'Reset Password',
