763735c94cf47d82683952ab8c2f61d74828787d
marco76tv <marco.sottana@gmail.com>
2023-04-05 14:46:21 +0000

    up

--- Http/Controllers/Auth/ForgotPasswordController.php
+++ Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\LU\Http\Controllers\Auth;
 
-<<<<<<< HEAD
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
-use Modules\LU\Http\Controllers\BaseController;
-use Modules\Xot\Services\FileService;
-=======
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Modules\Xot\Services\FileService;
 use Modules\LU\Http\Controllers\BaseController;
-use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
->>>>>>> be8aa77 (up)
+use Modules\Xot\Services\FileService;
 
 /**
  * Class ForgotPasswordController.
@@ -30,13 +23,7 @@ class ForgotPasswordController extends BaseController {
      * @return \Illuminate\View\View
      */
     public function showLinkRequestForm() {
-<<<<<<< HEAD
         $lang = app()->getLocale();
-=======
-
-        $lang = app()->getLocale();
-        
->>>>>>> be8aa77 (up)
         $piece = 'auth.passwords.email';
         FileService::viewCopy('lu::'.$piece, 'pub_theme::'.$piece);
 
