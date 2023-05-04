65a4b7f1fee37c5b24810b9981f6e55e1eb00c42
marco76tv <marco.sottana@gmail.com>
2023-05-04 17:59:56 +0200

    up

--- Services/ProfileService.php
+++ Services/ProfileService.php
@@ -811,12 +811,8 @@ class ProfileService {
 
             return $this->profile;
         }
-        // $profile = $this->profile;
-        // if (null == $profile) {
-        throw new \Exception('['.__LINE__.']['.__FILE__.']');
-        // }
 
-        // return $profile;
+        throw new \Exception('['.__LINE__.']['.__FILE__.']');
     }
 
 <<<<<<< HEAD
