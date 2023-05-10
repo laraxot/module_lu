b0b4d5f8ad2d8880688a2fd2a66190ef14227a3a
marco76tv <marco.sottana@gmail.com>
2023-05-10 12:13:58 +0200

    up

--- Http/Livewire/Modal/AddRole.php
+++ Http/Livewire/Modal/AddRole.php
@@ -7,7 +7,7 @@ namespace Modules\LU\Http\Livewire\Modal;
 use Illuminate\Contracts\Support\Renderable;
 use Modules\Cms\Actions\GetViewAction;
 use Modules\LU\Models\Role;
-use WireElements\Pro\Components\Modal\Modal;
+use Modules\Modal\View\Components\Modal\Modal;
 
 class AddRole extends Modal
 {
