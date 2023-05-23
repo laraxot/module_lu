784feb7b7c8a44af3858148c2aa89e00c1c066a1
marco76tv <marco.sottana@gmail.com>
2023-05-23 09:29:48 +0200

    rebase

--- bashscripts/composer_init.sh
+++ bashscripts/composer_init.sh
@@ -8,9 +8,12 @@ rm package-lock.json
 
 ##############  ####################
 php -d memory_limit=-1 composer.phar require -W illuminate/contracts
+<<<<<<< HEAD
 php -d memory_limit=-1 composer.phar require -W laraxot/module_xot
 php -d memory_limit=-1 composer.phar require -W laravel/ui
 php -d memory_limit=-1 composer.phar require -W laravel/passport
+=======
+>>>>>>> eebb6b6a (.)
 
 ############################ DEV ###############################
 php -d memory_limit=-1 composer.phar require -W --dev laravel/pint
