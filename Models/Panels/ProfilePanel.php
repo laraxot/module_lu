2dac8e988b85f909d0fe50025d21e0b4d6293af9
Malebestia <vair81@gmail.com>
2023-06-29 19:20:57 +0200

    .

--- Models/Panels/ProfilePanel.php
+++ Models/Panels/ProfilePanel.php
@@ -18,8 +18,7 @@ use Modules\LU\Models\User;
 /**
  * Class ProfilePanel.
  */
-class ProfilePanel extends XotBasePanel
-{
+class ProfilePanel extends XotBasePanel {
     /**
      * The model the resource corresponds to.
      */
@@ -38,13 +37,11 @@ class ProfilePanel extends XotBasePanel
     /**
      * The relationships that should be eager loaded on index queries.
      */
-    public function with(): array
-    {
+    public function with(): array {
         return [];
     }
 
-    public function search(): array
-    {
+    public function search(): array {
         return [];
     }
 
@@ -53,8 +50,7 @@ class ProfilePanel extends XotBasePanel
      *
      * @param Profile $row
      */
-    public function optionLabel($row): string
-    {
+    public function optionLabel($row): string {
         // [2022-08-17 13:06:20] local.ERROR: [162][Profile]
         // Access to an undefined property Illuminate\Database\Eloquent\Model::$user
         // if (null === $this->row->user) {
@@ -67,8 +63,7 @@ class ProfilePanel extends XotBasePanel
     /**
      * @return object[]
      */
-    public function fields(): array
-    {
+    public function fields(): array {
         return [
             (object) [
                 'type' => 'Id',
@@ -125,8 +120,7 @@ class ProfilePanel extends XotBasePanel
     /**
      * Get the tabs available.
      */
-    public function tabs(): array
-    {
+    public function tabs(): array {
         $tabs_name = [];
 
         return [];
@@ -135,32 +129,28 @@ class ProfilePanel extends XotBasePanel
     /**
      * Get the cards available for the request.
      */
-    public function cards(Request $request): array
-    {
+    public function cards(Request $request): array {
         return [];
     }
 
     /**
      * Get the filters available for the resource.
      */
-    public function filters(Request $request = null): array
-    {
+    public function filters(Request $request = null): array {
         return [];
     }
 
     /**
      * Get the lenses available for the resource.
      */
-    public function lenses(Request $request): array
-    {
+    public function lenses(Request $request): array {
         return [];
     }
 
     /**
      * Get the actions available for the resource.
      */
-    public function actions(Request $request = null): array
-    {
+    public function actions(Request $request = null): array {
         return [
             // new \Modules\Blog\Models\Panels\Actions\PersonalInfoAction(),
             // new \Modules\Blog\Models\Panels\Actions\UserSecurityAction(),
@@ -172,13 +162,9 @@ class ProfilePanel extends XotBasePanel
     /**
      * @param int $size
      */
-    public function avatar($size = 100): ?string
-    {
+    public function avatar($size = 100): ?string {
         if (null === $this->row) {
             throw new \Exception('row is null');
-        }
-        if (! property_exists($this->row, 'user')) {
-            throw new \Exception('in ['.\get_class($this->row).'] property [user] not exists');
         }
         /** @var \Modules\LU\Models\User */
         $user = $this->row->user;
@@ -198,8 +184,7 @@ class ProfilePanel extends XotBasePanel
         return "https://www.gravatar.com/avatar/$email?d=$default&s=$size";
     }
 
-    public function storeCallback(array $params): object
-    {
+    public function storeCallback(array $params): object {
         extract($params);
         /*
         * metto apposto il titolo della pagina del profilo
@@ -262,8 +247,7 @@ class ProfilePanel extends XotBasePanel
     }
     */
 
-    public function isSuperAdmin(): bool
-    {
+    public function isSuperAdmin(): bool {
         // 232 Access to an undefined property Illuminate\Database\Eloquent\Model::$user.
         // $user = $this->row->user;
         // $user = $this->row->getRelationValue('user');
